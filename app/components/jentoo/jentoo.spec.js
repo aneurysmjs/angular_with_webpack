a6fbf6678479b1a5e9ce051c0fec891560811690
blackendstudios <jeroflat@gmail.com>
2015-12-03 16:24:39 -0500

    jasmine configured,  basic test working

--- app/components/jentoo/jentoo.spec.js
+++ app/components/jentoo/jentoo.spec.js
@@ -1,20 +1,13 @@
-describe('Jentoo Component');
+describe('Jentoo Component', function () {
 
-// Instantiate a new version of my module before each test
-beforeEach(module('notesApp'));
-var ctrl;
-// Before each unit test, instantiate a new instance
-// of the controller
-beforeEach(inject(function ($controller) {
-   ctrl = $controller('JentooController');
-   console.log('--- ctrl ---');
-   console.log(ctrl);
-}));
+   var t;
 
-/*
-it('should have items available on load', function () {
-   expect(ctrl.items).toEqual([
-      {id: 1, label: 'First', done: true},
-      {id: 2, label: 'Second', done: false}
-   ]);
-});*/
+   beforeEach(() => {
+      t = true;
+   });
+
+   it('t should be true', () => {
+      expect(t).toBeTruthy();
+   });
+
+});
